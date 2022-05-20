0b5f9e611539194cec67d40bd8a6519f5aae993d
akhilbesfriendmummy <74089296+akhilbesfriendmummy@users.noreply.github.com>
2022-05-20 02:07:32 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+//https://teachablemachine.withgoogle.com/models/tZBDvmqNc/
+
+Webcam.set({
+    width:350,
+    height:300,
+    image_format:'png',
+    png_quality:90
+});
+camera=document.getElementById("camera");
+Webcam.attach('#camera');
+function take_snapshot(){
+    Webcam.snap(function(data_uri){
+        document.getElementById("result").innerHTML="<img id='captured_image' src='"+data_uri+"'>";
+    })
+}
+classifier=ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/tZBDvmqNc/model.json',modelLoaded);
